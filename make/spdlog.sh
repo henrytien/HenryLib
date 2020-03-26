56437f8470e00d0cb4399d1b61b942248df1e37e
henrytine <tianhangyu8886@gmail.com>
2020-03-27 00:18:18 +0800

    spdlog-lib
    learn how to use spdlog

---
+++ make/spdlog.sh
@@ -0,0 +1,4 @@
+
+# spdlog
+g++ -o ../bin/spdlog -std=c++11 ../src/spdlog.cpp -I "../include"
+
---
+++ src/spdlog.cpp
@@ -0,0 +1,10 @@
+
+
+#include <iostream>
+#include "../include/spdlog/spdlog.h"
+int main(){
+	spdlog::info("Welcome to spdlog!");
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
+
